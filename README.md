a66ceffa3f98b8f865b595388c1f752412772a04
karlenko <vitamin6754@gmail.com>
2024-03-07 01:02:01 +0200

    update the shell script

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 ```shell
 source java_run.sh
 ```
+
+## Grep only real time values
+```shell
+source java_run.sh 2>&1 | grep real >> output.txt
+```
--- java_run.sh
+++ java_run.sh
@@ -1,7 +1,22 @@
-time /home/karlenko/.jdks/openjdk-21.0.1/bin/java -javaagent:/snap/intellij-idea-ultimate/478/lib/idea_rt.jar=36585:/snap/intellij-idea-ultimate/478/bin -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -classpath /home/karlenko/IdeaProjects/lab1/out/production/lab1:/home/karlenko/.m2/repository/com/anaptecs/jeaf/owalibs/org.apache.commons.cli/4.3.1/org.apache.commons.cli-4.3.1.jar Main -v --thread 2 --time 2 --step 1
-echo '==================================='
-time /home/karlenko/.jdks/openjdk-21.0.1/bin/java -javaagent:/snap/intellij-idea-ultimate/478/lib/idea_rt.jar=36585:/snap/intellij-idea-ultimate/478/bin -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -classpath /home/karlenko/IdeaProjects/lab1/out/production/lab1:/home/karlenko/.m2/repository/com/anaptecs/jeaf/owalibs/org.apache.commons.cli/4.3.1/org.apache.commons.cli-4.3.1.jar Main -v --thread 8 --time 5 --step 5
-echo '==================================='
-time /home/karlenko/.jdks/openjdk-21.0.1/bin/java -javaagent:/snap/intellij-idea-ultimate/478/lib/idea_rt.jar=36585:/snap/intellij-idea-ultimate/478/bin -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -classpath /home/karlenko/IdeaProjects/lab1/out/production/lab1:/home/karlenko/.m2/repository/com/anaptecs/jeaf/owalibs/org.apache.commons.cli/4.3.1/org.apache.commons.cli-4.3.1.jar Main -v --thread 16 --time 10 --step 10
-echo '==================================='
-time /home/karlenko/.jdks/openjdk-21.0.1/bin/java -javaagent:/snap/intellij-idea-ultimate/478/lib/idea_rt.jar=36585:/snap/intellij-idea-ultimate/478/bin -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -classpath /home/karlenko/IdeaProjects/lab1/out/production/lab1:/home/karlenko/.m2/repository/com/anaptecs/jeaf/owalibs/org.apache.commons.cli/4.3.1/org.apache.commons.cli-4.3.1.jar Main -v --thread 32 --time 20 --step 50
+#!/bin/bash
+
+options=(
+    "-v --thread 2 --time 2 --step 1"
+    "-v --thread 8 --time 5 --step 5"
+    "-v --thread 16 --time 10 --step 10"
+    "-v --thread 32 --time 20 --step 50"
+)
+
+base_command() {
+    /home/karlenko/.jdks/openjdk-21.0.1/bin/java -javaagent:/snap/intellij-idea-ultimate/478/lib/idea_rt.jar=36585:/snap/intellij-idea-ultimate/478/bin -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -classpath /home/karlenko/IdeaProjects/lab1/out/production/lab1:/home/karlenko/.m2/repository/com/anaptecs/jeaf/owalibs/org.apache.commons.cli/4.3.1/org.apache.commons.cli-4.3.1.jar Main "$@"
+}
+
+print_verbose_results() {
+    for i in "${options[@]}"; 
+    do
+        time base_command ${i}
+        echo "==================================="
+    done
+}
+
+print_verbose_results
